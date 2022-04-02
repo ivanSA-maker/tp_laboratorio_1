c0d61cde4165983d3e59e337a538891bd7fbe891
lccarelli <carelli.lc@gmail.com>
2022-04-02 17:51:52 -0300

    primer commit

---
+++ TP_[01]/src/TP_[01].c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TP_[01].c
+ Author      : Iván Wesley Serna Alegre
+ Version     : 1.0
+ Description :
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+	return EXIT_SUCCESS;
+}
